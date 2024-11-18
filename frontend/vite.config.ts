4abe174ce1aa2abf81c904e8a83723a099647788
aniveam <vinajilliane@gmail.com>
2024-11-17 18:37:14 -0800

    config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-    	"@": path.resolve(__dirname, "src"),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
